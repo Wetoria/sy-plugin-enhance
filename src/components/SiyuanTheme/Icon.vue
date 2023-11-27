2a4f8647da75a23c4d894eb0793065f756bb3588
Vip <vwetoria@gmail.com>
2023-11-27 23:34:44 +0800

    feat: 增加思源主题icon组件

---
+++ src/components/SiyuanTheme/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    :style="{
+      fontSize: size,
+      width: size,
+      height: size,
+    }"
+  >
+    <use :xlink:href="`#${name}`"></use>
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: String,
+  size: {
+    type: [Number, String],
+    default: 12
+  },
+})
+</script>
