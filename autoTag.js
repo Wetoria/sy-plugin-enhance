4d9dde61109897d89ef84172eea4a578c1a2b1ea
Vip <vwetoria@gmail.com>
2023-12-07 19:57:27 +0800

    feat: 增加tag相关命令

---
+++ autoTag.js
@@ -0,0 +1,12 @@
+import { exec } from 'child_process'
+import PluginInfo from './plugin.json' assert { type: "json" }
+
+const {
+  version
+} = PluginInfo
+
+exec(`git tag v${version}`, (err, stdout) => {
+  if (!err) {
+    exec(`git push origin v${version}`)
+  }
+})
