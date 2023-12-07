aa0d592b000ed2773148d43749cc2b98a860ba47
Vip <vwetoria@gmail.com>
2023-12-07 20:02:30 +0800

    feat: 优化tag脚本

--- autoTag.js
+++ autoTag.js
@@ -8,5 +8,9 @@ const {
 exec(`git tag v${version}`, (err, stdout) => {
   if (!err) {
     exec(`git push origin v${version}`)
+  } else {
+    console.log()
+    console.error('Error is: ', err)
+    console.log(stdout)
   }
 })
