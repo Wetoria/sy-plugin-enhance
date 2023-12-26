f3f2160828fd91b9020313103ebc3bd3ac5e08fa
Vip <vwetoria@gmail.com>
2023-12-26 12:02:35 +0800

    feat: 增加LifeLog颜色配置

--- src/components/LifeLog/LifeLog.vue
+++ src/components/LifeLog/LifeLog.vue
@@ -27,15 +27,22 @@ html[data-enhancer-enable-lifelog-tag="true"] {
     &[custom-lifelog-type="固定"] {
       border-left: 1px solid #D3D3D3;
     }
+
     &[custom-lifelog-type="增"] {
       border-left: 1px solid #90EE90;
     }
     &[custom-lifelog-type="学习"] {
       border-left: 1px solid #90EE90;
     }
+
     &[custom-lifelog-type="事业"] {
+      border-left: 1px solid #90EE90;
+    }
+
+    &[custom-lifelog-type="工作"] {
       border-left: 1px solid #FFD700;
     }
+
     &[custom-lifelog-type="废"] {
       border-left: 1px solid #FF0000;
     }
@@ -48,6 +55,20 @@ html[data-enhancer-enable-lifelog-tag="true"] {
     &[custom-lifelog-type="玩"] {
       border-left: 1px solid #FF0000;
     }
+
+    &[custom-lifelog-type="家庭"] {
+      border-left: 1px solid rgb(71, 255, 248);
+    }
+    &[custom-lifelog-type="家"] {
+      border-left: 1px solid rgb(71, 255, 248);
+    }
+    &[custom-lifelog-type="朋友"] {
+      border-left: 1px solid rgb(156, 123, 85);
+    }
+    &[custom-lifelog-type="友"] {
+      border-left: 1px solid rgb(156, 123, 85);
+    }
+
   }
 }
 </style>
