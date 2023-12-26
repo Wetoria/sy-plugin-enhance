cf83defbafb9407d39eb88125d538c365e9a58c5
Vip <vwetoria@gmail.com>
2023-12-26 12:26:06 +0800

    imp: 调整LifeLog的标记逻辑

--- src/components/LifeLog/logic.ts
+++ src/components/LifeLog/logic.ts
@@ -40,12 +40,13 @@ export function markLifeLogBlock() {
 
       const content = opt.text
 
-      const isLifeLogParagraph = /^\d{2}:\d{2}\s+/.test(content)
+      // const isLifeLogParagraph = /^\d{2}:\d{2}\s+/.test(content)
+      const isLifeLogParagraph = /^\d{2}:\d{2}\s+.*?🅻$/.test(content)
       if (!isLifeLogParagraph) {
         return
       }
       const time = (content.match(/^\d{2}:\d{2}/) || [])[0]
-      const elseContent = content.replace(/^\d{2}:\d{2}\s+/, '')
+      const elseContent = content.replace(/^\d{2}:\d{2}\s+/, '').replace('🅻', '')
 
       if (!elseContent) {
         return
