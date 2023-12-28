d697d476a757f6e7b57ec38d1040abf87966c63b
Vip <vwetoria@gmail.com>
2023-12-28 15:53:15 +0800

    feat: 增加思源platform信息

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,12 @@ import { destroy, init } from './main';
 
 export default class VPlugin extends Plugin {
   public isMobile: boolean;
+  public platform: string;
   public readonly version = version;
 
   async onload() {
     const frontEnd = getFrontend();
+    this.platform = frontEnd
     this.isMobile = frontEnd === "mobile" || frontEnd === "browser-mobile";
 
     init(this);
