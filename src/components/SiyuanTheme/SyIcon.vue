e26e5f8962bd124486f3418fa6ed8861c433f62e
Vip <vwetoria@gmail.com>
2024-04-13 17:12:59 +0800

    imp: 调整icon的容器效果

--- src/components/SiyuanTheme/SyIcon.vue
+++ src/components/SiyuanTheme/SyIcon.vue
@@ -1,15 +1,17 @@
 <template>
-  <svg
-    :style="{
-      fontSize: size,
-      width: size,
-      height: size,
-      color: disabled ? 'var(--b3-empty-color)' : undefined
-    }"
-    class="enSyIcon"
-  >
-    <use :xlink:href="`#${name}`"></use>
-  </svg>
+  <div class="enIconContainer">
+    <svg
+      :style="{
+        fontSize: size,
+        width: size,
+        height: size,
+        color: disabled ? 'var(--b3-empty-color)' : undefined
+      }"
+      class="enSyIcon"
+    >
+      <use :xlink:href="`#${name}`"></use>
+    </svg>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,3 +24,11 @@ defineProps({
   disabled: Boolean,
 })
 </script>
+
+<style lang="scss" scoped>
+.enIconContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
