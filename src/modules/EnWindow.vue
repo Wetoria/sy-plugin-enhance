baaa0d06b18b1bd159aa677814fb53819d974d0e
Vip <vwetoria@gmail.com>
2024-05-12 12:07:07 +0800

    feat: 增加EnWindow组件

---
+++ src/modules/EnWindow.vue
@@ -0,0 +1,305 @@
+<template>
+  <Teleport
+    to="html"
+  >
+    <div
+      class="enWindowContainer flexColumn"
+      :data-in-window="isInWindow"
+      v-if="isInWindow"
+    >
+      <div class="windowToolBarArea">
+        <div
+          class="windowDraggerArea"
+        >
+
+        </div>
+        <div class="btns">
+          <div
+            class="toolBarItem" @click="switchPinStatus"
+            v-if="isInWindow"
+          >
+            <SyIcon v-if="pinned" name="iconUnpin" size="16" />
+            <SyIcon v-else name="iconPin" size="16" />
+          </div>
+          <div
+            class="toolBarItem"
+            @click="hideWindow"
+          >
+            <icon-close class="closeIcon" />
+          </div>
+        </div>
+      </div>
+      <div class="displayArea">
+        <slot></slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+
+<script setup lang="ts">
+import SyIcon from '@/components/SiyuanTheme/SyIcon.vue'
+import { usePlugin } from '@/main'
+import { SyFrontendTypes } from '@/utils/Siyuan'
+import { onMounted, ref } from 'vue'
+
+const props = defineProps<{
+  inWindow: boolean;
+  windowTitle: string;
+  createImmediate?: boolean;
+}>()
+
+const emit = defineEmits([
+  'update:visible',
+  'update:inWindow',
+])
+
+const enWindowKey = `enWindow-${props.windowTitle}`
+
+const urlSearchParams = new URLSearchParams(location.search)
+const isInWindow = urlSearchParams.has('enhance') && urlSearchParams.get('enhance') === 'true'
+  && urlSearchParams.has('enWindowTitle') && urlSearchParams.get('enWindowTitle') === props.windowTitle
+
+let winRef = null
+
+const createEnWindow = () => {
+  winRef = createWindow(props.windowTitle)
+  return winRef
+}
+const getEnWinRef = () => {
+  if (!winRef) {
+    if (isInWindow) {
+      winRef = getWindow(props.windowTitle, isInWindow)
+    } else {
+      winRef = createEnWindow()
+    }
+  }
+  return winRef
+}
+
+const saveInitData = () => {
+  localStorage.setItem(enWindowKey, JSON.stringify({
+    pinned: pinned.value,
+  }))
+}
+
+const loadInitData = () => {
+  const str = localStorage.getItem(enWindowKey)
+  try {
+    const obj = JSON.parse(str)
+    updateDataByStorage(obj)
+  } catch(err) {}
+}
+const updateDataByStorage = (obj) => {
+  pinned.value = obj.pinned
+  emit('update:visible', obj.visible)
+}
+onMounted(() => {
+  // addEventListener('storage', (event) => {
+  //   if (event.key === enWindowKey) {
+  //     const obj = JSON.parse(event.newValue)
+  //     updateDataByStorage(obj)
+  //   }
+  // })
+})
+
+const openWindow = () => {
+  saveInitData()
+  const win = getEnWinRef()
+  win.show()
+}
+
+const hideWindow = () => {
+  getEnWinRef()?.hide();
+}
+
+const switchWindowVisible = (pin: boolean) => {
+  const win = getEnWinRef()
+  if (win.isVisible()) {
+    hideWindow()
+  } else {
+    win.setAlwaysOnTop(pin)
+    openWindow()
+  }
+}
+
+
+window.addEventListener('beforeunload', () => {
+  if (winRef) {
+    winRef.hide()
+    winRef.destroy()
+  }
+})
+
+const pinned = ref(false)
+const switchPinStatus = () => {
+  pinWindow(!pinned.value)
+}
+const pinWindow = (value) => {
+  pinned.value = value
+  getEnWinRef().setAlwaysOnTop(pinned.value)
+}
+defineExpose({
+  pinWindow,
+  openWindow,
+  hideWindow,
+  switchWindowVisible,
+  isVisible: () => getEnWinRef().isVisible(),
+  getWin: () => {
+    return getEnWinRef()
+  }
+});
+
+onMounted(() => {
+  if (isInWindow) {
+    document.documentElement.dataset.enWindow = "true"
+
+    winRef = getWindow(props.windowTitle, isInWindow)
+
+    loadInitData()
+  } else {
+    if (props.createImmediate) {
+      createEnWindow()
+    }
+  }
+})
+</script>
+
+<script lang="ts">
+
+const cannot = () => {
+  const canPlatform = [SyFrontendTypes.desktop, SyFrontendTypes['desktop-window']]
+  const plugin = usePlugin()
+  return !canPlatform.includes(plugin.platform)
+}
+
+export const createWindow = (title, queryStr?) => {
+  if (cannot()) {
+    return
+  }
+  const { BrowserWindow } = require('@electron/remote')
+
+  const electronWindow = new BrowserWindow({
+    width: 1000,
+    height: 350,
+    show: false,
+    resizable: true,
+    movable: true,
+    frame: false,
+    title: title,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+      webviewTag: true,
+      webSecurity: false,
+      nativeWindowOpen: true,
+    }
+  })
+
+  require("@electron/remote")
+      .require("@electron/remote/main")
+      .enable(electronWindow.webContents);
+
+  electronWindow.setTitle(title)
+
+  electronWindow.loadURL(
+    `${window.location.protocol}//${window.location.host}/stage/build/app/window.html?enhance=true&enWindowTitle=${title}&${queryStr ? `&${queryStr}` : ''}`
+  )
+  return electronWindow
+}
+
+export function getWindow(title, isInWindow = false) {
+  if (cannot() && !isInWindow) {
+    return
+  }
+  require('@electron/remote/main').enable(window);
+  const { BrowserWindow } = require('@electron/remote');
+  const allWindows = BrowserWindow.getAllWindows();
+  const specificWindow = allWindows.find(window => {
+    // 这里可以根据窗口的属性或其他特征来判断是否是你需要的窗口
+    return window.getTitle() === title;
+  });
+  return specificWindow
+}
+
+</script>
+
+<style lang="scss">
+.enWindowContainer {
+  width: 500px;
+  height: 300px;
+  background-color: var(--b3-theme-background);
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  opacity: var(--en-opacity);
+
+  display: flex;
+
+  &[data-in-window="true"] {
+    .windowDraggerArea {
+      -webkit-app-region: drag;
+    }
+  }
+  .windowToolBarArea {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+
+    .windowDraggerArea {
+      flex: 1;
+    }
+
+    .btns {
+      display: flex;
+      box-sizing: border-box;
+      padding: 12px;
+      gap: var(--en-gap);
+
+      .toolBarItem {
+        width: 24px;
+        height: 24px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+
+        .arco-icon,
+        .enSyIcon {
+          color: var(--b3-toolbar-color);
+          font-size: 16px;
+        }
+
+        &:hover {
+          background-color: var(--b3-toolbar-hover);
+
+          .arco-icon,
+          .enSyIcon {
+            color: var(--b3-theme-on-background);
+            font-size: 16px;
+          }
+        }
+      }
+    }
+
+  }
+
+  .displayArea {
+    flex: 1;
+  }
+}
+</style>
+
+<style lang="scss">
+html[data-en-window="true"] {
+  .toolbar__window,
+  #status {
+    display: none;
+  }
+
+  .enWindowContainer {
+    width: 100vw;
+    height: 100vh;
+  }
+}
+</style>
