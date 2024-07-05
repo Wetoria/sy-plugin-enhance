437f309a70c99207caf72903c09e98881ccc793b
Vip <vwetoria@gmail.com>
2024-07-05 08:23:12 +0800

    imp: 处理项目自动引入的问题

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,15 @@ module.exports = {
   root: true,
   extends: [
     "eslint:recommended",
-    'plugin:vue/vue3-essential',
+    "plugin:vue/vue3-essential",
     "plugin:@typescript-eslint/recommended",
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    "turbo",
-    "prettier",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
 
   parser: "@typescript-eslint/parser",
 
-  overrides: [
-  ],
+  overrides: [],
 
   plugins: [
     "vue",
@@ -33,12 +30,5 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "turbo/no-undeclared-env-vars": "off",
     "prettier/prettier": "error",
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": 1, // 单行元素每个属性都要放在新行
-      "multiline": {
-        "max": 1, // 多行元素每个属性都要放在新行
-        "allowFirstLine": false // 不允许在第一行写属性
-      }
-    }]
   },
-}
+};
