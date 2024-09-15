14f509652f0a7fdb3f4c98f0e30b1c933093a559
Vip <vwetoria@gmail.com>
2024-09-15 15:33:06 +0800

    feat: 增加 Electron 标记

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ export default class EnhancePlugin extends Plugin {
   public isMobile: boolean;
   public isBrowser: boolean;
   public isLocal: boolean;
+  public isElectron: boolean;
   public platform: SyFrontendTypes;
   public readonly version = version;
 
@@ -32,6 +33,14 @@ export default class EnhancePlugin extends Plugin {
     this.isMobile = frontEnd === "mobile" || frontEnd === "browser-mobile";
     this.isBrowser = frontEnd.includes('browser')
     this.isLocal = location.href.includes('127.0.0.1') || location.href.includes('localhost')
+    console.log('require is ', require)
+    try {
+      require("@electron/remote")
+      .require("@electron/remote/main")
+      this.isElectron = true
+    } catch(err) {
+      this.isElectron = false
+    }
 
     init(this);
   }
