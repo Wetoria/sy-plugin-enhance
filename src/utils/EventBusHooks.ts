a67b9dc827434084748d2ccbd58e03ec7335debd
Vip <vwetoria@gmail.com>
2024-10-26 08:39:06 +0800

    imp: 调整方法名

--- src/utils/EventBusHooks.ts
+++ src/utils/EventBusHooks.ts
@@ -1,7 +1,7 @@
 import { usePlugin } from '@/main'
 import { IEventBusMap } from 'siyuan'
 
-export function useEventBus(event: keyof IEventBusMap, cb: (event: any) => void) {
+export function useSiyuanEvent(event: keyof IEventBusMap, cb: (event: any) => void) {
   const plugin = usePlugin()
   plugin.eventBus.on(event, (event) => {
     cb(event)
@@ -11,16 +11,23 @@ export function useEventBus(event: keyof IEventBusMap, cb: (event: any) => void)
   }
 }
 
-export function useLoadedProtyleStatic(cb: (event: any) => void) {
-  return useEventBus('loaded-protyle-static', cb)
+export function useSiyuanEventOnce(event: keyof IEventBusMap, cb: (event: any) => void) {
+  const plugin = usePlugin()
+  plugin.eventBus.once(event, (event) => {
+    cb(event)
+  })
 }
 
-export function useWsMain(cb: (event: any) => void) {
-  return useEventBus('ws-main', cb)
+export function useSiyuanEventLoadedProtyleStatic(cb: (event: any) => void) {
+  return useSiyuanEvent('loaded-protyle-static', cb)
 }
 
-export function useTransactions(cb: (event: any) => void) {
-  return useWsMain((event) => {
+export function useSiyuanEventWsMain(cb: (event: any) => void) {
+  return useSiyuanEvent('ws-main', cb)
+}
+
+export function useSiyuanEventTransactions(cb: (event: any) => void) {
+  return useSiyuanEventWsMain((event) => {
     const { detail } = event
     if (detail.cmd === 'transactions') {
       cb(event)
