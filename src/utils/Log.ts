8fc59612abb60ef9574f63cfc531dc5127f88ce2
Vip <vwetoria@gmail.com>
2024-11-05 08:23:39 +0800

    chore: 增加日志内容

--- src/utils/Log.ts
+++ src/utils/Log.ts
@@ -1,12 +1,16 @@
+const prefix = '[SEP by Wetoria]'
 export function log(...args) {
-  return console.log('%c[Enhance] ', 'background-color: gray; color: white;', ...args)
+  return console.log(`%c${prefix} `, 'background-color: gray; color: white;', ...args)
 }
 window.enLog = log
+enLog('测试 Log')
 export function warn(...args) {
-  return console.warn('%c[Enhance] ', 'background-color: yellow; color: black;', ...args)
+  return console.warn(`%c${prefix} `, 'background-color: yellow; color: black;', ...args)
 }
 window.enWarn = warn
+enWarn('测试 Warn')
 export function error(...args) {
-  return console.error('%c[Enhance] ', 'background-color: red; color: white;', ...args)
+  return console.error(`%c${prefix} `, 'background-color: red; color: white;', ...args)
 }
 window.enError = error
+enError('测试 Error')
