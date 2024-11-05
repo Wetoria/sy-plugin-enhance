071a98a0311a6ce2ed333a7396fc965ee0f53885
Vip <vwetoria@gmail.com>
2024-11-05 08:17:01 +0800

    chore: 浏览器log时，增加颜色效果用作区分

--- src/utils/Log.ts
+++ src/utils/Log.ts
@@ -1,12 +1,12 @@
 export function log(...args) {
-  return console.log('[Enhance] ', ...args)
+  return console.log('%c[Enhance] ', 'background-color: gray; color: white;', ...args)
 }
 window.enLog = log
 export function warn(...args) {
-  return console.warn('[Enhance] ', ...args)
+  return console.warn('%c[Enhance] ', 'background-color: yellow; color: black;', ...args)
 }
 window.enWarn = warn
 export function error(...args) {
-  return console.error('[Enhance] ', ...args)
+  return console.error('%c[Enhance] ', 'background-color: red; color: white;', ...args)
 }
 window.enError = error
