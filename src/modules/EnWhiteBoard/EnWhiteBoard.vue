f0cbe2efaaf658cda24b4da792af3f6fa7c5809a
Vip <vwetoria@gmail.com>
2024-11-27 18:49:19 +0800

    feat: slash菜单创建带白板标记的html块

--- src/modules/EnWhiteBoard/EnWhiteBoard.vue
+++ src/modules/EnWhiteBoard/EnWhiteBoard.vue
@@ -2,9 +2,41 @@
   <EnWhiteBoardEntry />
 </template>
 
-<script setup lang="ts">
-import EnWhiteBoardEntry from './EnWhiteBoardEntry.vue';
+<script lang="ts">
+export interface EnWhiteBoardIndexMap {
+  [key: string]: {
+    whiteBoardId: string
+    whiteBoardName: string
+  }
+}
 
+const whiteBoardIndexMap = useSyncModuleData<EnWhiteBoardIndexMap>({
+  namespace: 'enWhiteBoardIndexMap',
+  defaultData: {},
+})
+
+// TODO 获取白板的逻辑
+export function getWhiteBoardListBySearchValue(searchValue: string) {
+  const testData = [
+  ]
+
+  for (let i = 1; i < 21; i++) {
+    testData.push({
+      whiteBoardId: `en-whiteboard-id-${i}`,
+      whiteBoardName: `白板 ${i}`,
+    })
+  }
+
+  return testData.filter(item => {
+    const included = item.whiteBoardName.includes(searchValue) || item.whiteBoardId.includes(searchValue)
+    return included
+  })
+}
+</script>
+
+<script setup lang="ts">
+import { useSyncModuleData } from '@/utils/SyncData';
+import EnWhiteBoardEntry from './EnWhiteBoardEntry.vue';
 
 </script>
 
--- src/utils/Siyuan/utils.ts
+++ src/utils/Siyuan/utils.ts
@@ -1,3 +1,5 @@
+import { usePlugin } from '@/main';
+
 const isNormalItem = (currentHintElement: HTMLElement, className: string) => {
   return currentHintElement.classList.contains("fn__none") || !currentHintElement.classList.contains(className);
 };
@@ -95,3 +97,34 @@ export const upDownHint = (listElement: Element, event: KeyboardEvent, classActi
       return currentHintElement;
   }
 };
+
+
+export const setPosition = (element: HTMLElement, x: number, y: number, targetHeight = 0, targetLeft = 0) => {
+  const plugin = usePlugin()
+  const SIZE_TOOLBAR_HEIGHT = plugin.isMobile ? 0 : 32
+  element.style.top = y + "px";
+  element.style.left = x + "px";
+  const rect = element.getBoundingClientRect();
+  // 上下超出屏幕
+  if (rect.bottom > window.innerHeight || rect.top < SIZE_TOOLBAR_HEIGHT) {
+      const top = y - rect.height - targetHeight;
+      if (top > SIZE_TOOLBAR_HEIGHT && (top + rect.height) < window.innerHeight) {
+          // 上部
+          element.style.top = top + "px";
+      } else if (top <= SIZE_TOOLBAR_HEIGHT) {
+          // 位置超越到屏幕上方外时，需移动到屏幕顶部。eg：光标在第一个块，然后滚动到上方看不见的位置，按 ctrl+a
+          element.style.top = SIZE_TOOLBAR_HEIGHT + "px";
+      } else {
+          // 依旧展现在下部，只是位置上移
+          element.style.top = Math.max(SIZE_TOOLBAR_HEIGHT, window.innerHeight - rect.height) + "px";
+      }
+  }
+  if (rect.right > window.innerWidth) {
+      // 展现在左侧
+      element.style.left = `${window.innerWidth - rect.width - targetLeft}px`;
+  } else if (rect.left < 0) {
+      // 依旧展现在左侧，只是位置右移
+      element.style.left = "0";
+  }
+};
+
