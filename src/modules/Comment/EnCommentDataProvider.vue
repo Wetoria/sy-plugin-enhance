7ba0e85a6409148bbc6ce3b355e0bef17cb10a83
Vip <vwetoria@gmail.com>
2025-04-05 17:31:29 +0800

    imp: 优化批注数据的查询逻辑

--- src/modules/Comment/EnCommentDataProvider.vue
+++ src/modules/Comment/EnCommentDataProvider.vue
@@ -8,27 +8,71 @@ import {
   EN_COMMENT_KEYS,
   provideCommentIdList,
 } from '@/modules/Comment/Comment'
+import { injectGlobalWindowData } from '@/modules/EnModuleControl/ModuleProvide'
 import { debounce } from '@/utils'
 import { useSiyuanEventTransactions } from '@/utils/EventBusHooks'
 import {
+  computed,
   onBeforeUnmount,
   onMounted,
   ref,
+  watch,
 } from 'vue'
 
 
 const commentIdList = ref([])
 provideCommentIdList(commentIdList)
 
+const globalWindowData = injectGlobalWindowData()
+const protyleContentRefList = computed<IProtyleObserverItem[]>(() => {
+  return globalWindowData.value.protyleList
+})
+
 const getAllCommentIds = async () => {
+  const protyleBlockIdList = protyleContentRefList.value.map((i) => i.protyleBlockId)
+
+  if (!protyleBlockIdList.length) {
+    commentIdList.value = []
+    return
+  }
+
   const sqlStmt = `
+    with comment_ids as (
+      select distinct
+        CASE
+          WHEN b.type = 'd' THEN b.id
+          ELSE b.root_id
+        END as id
+      from
+        blocks b
+      where
+        id in (${protyleBlockIdList.map((i) => `'${i}'`).join(',')})
+      union
+      select distinct
+        b.id
+      from
+        blocks b
+      where
+        root_id in (
+          select
+            CASE
+              WHEN type = 'd' THEN id
+              ELSE root_id
+            END
+          from
+            blocks
+          where
+            id in (${protyleBlockIdList.map((i) => `'${i}'`).join(',')})
+        )
+    )
     select
-      *
+      a.*
     from
-      attributes
+      attributes a
+      inner join comment_ids c
     where
-      name = '${EN_COMMENT_KEYS.nodeCommentRefKey}'
-      and value like '${EN_COMMENT_KEYS.commentIdPrefix}-%'
+      a.name = '${EN_COMMENT_KEYS.nodeCommentRefKey}'
+      and a.value like '${EN_COMMENT_KEYS.commentIdPrefix}-' || c.id || '-%'
     limit
       9999999
   `
@@ -36,10 +80,16 @@ const getAllCommentIds = async () => {
   commentIdList.value = res.map((i) => i.value)
 }
 
+
+
 const debouncedGetAllCommentIds = debounce(getAllCommentIds, 1000)
 
+watch(protyleContentRefList, () => {
+  debouncedGetAllCommentIds()
+}, { immediate: true })
+
 onMounted(() => {
-  getAllCommentIds()
+  debouncedGetAllCommentIds()
 })
 
 const offTransactions = useSiyuanEventTransactions(() => {
